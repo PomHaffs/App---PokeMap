ca7d0ada992d929cf40f4ea385db7aa381459214
PomHaffs <tomashaffenden@icloud.com>
2016-11-25 10:24:50 +1100

    Added GeoFire and bridging header

---
+++ PokeMap/PokeMap-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  PokeMap-Bridging-Header.h
+//  PokeMap
+//
+//  Created by Tomas-William Haffenden on 25/11/16.
+//  Copyright © 2016 PomHaffs. All rights reserved.
+//
+
+//Had to create this file inorder to deal with GeoFire and that fact it is writen in Obj-C
+
+#import "GeoFire.h"
